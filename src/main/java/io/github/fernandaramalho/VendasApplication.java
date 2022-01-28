5742a9d22be42bb3aab58ba2fcecabf4af44b006
Fernanda Ramalho <fernandadosantos021999@gmail.com>
2022-01-28 17:01:45 -0300

    Introduction Spring Boot annotation

--- src/main/java/io/github/fernandaramalho/VendasApplication.java
+++ src/main/java/io/github/fernandaramalho/VendasApplication.java
@@ -1,5 +1,7 @@
 package io.github.fernandaramalho;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,10 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class VendasApplication {
+    @Autowired
+    @Qualifier("applicationName")
+    private String applicationName;
 
     @GetMapping("/hello")
     public String HelloWorld() {
-        return "Hello World Fernandinha!!!";
+        return applicationName;
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/io/github/fernandaramalho/service/MyService.java
@@ -0,0 +1,7 @@
+package io.github.fernandaramalho.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyService {
+}
